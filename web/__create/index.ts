ceaee3ac8f5dcd78cba85bce6963bef722d8cb81
lunos-ai <dev.lunos.ai@gmail.com>
2025-11-15 22:33:40 +0300

    Updated index.ts

--- web/__create/index.ts
+++ web/__create/index.ts
@@ -4,7 +4,7 @@ import { skipCSRFCheck } from '@auth/core';
 import Credentials from '@auth/core/providers/credentials';
 import { authHandler, initAuthConfig } from '@hono/auth-js';
 import { Pool, neonConfig } from '@neondatabase/serverless';
-import { hash, verify } from 'argon2';
+import * as bcrypt from 'bcryptjs'
 import { Hono } from 'hono';
 import { contextStorage, getContext } from 'hono/context-storage';
 import { cors } from 'hono/cors';
