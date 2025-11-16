f6283ae5c48c9e7356e0d54ac024ccc56d67e4c8
lunos-ai <dev.lunos.ai@gmail.com>
2025-11-16 10:19:33 +0300

    Updated sql.js

--- web/src/app/api/utils/sql.js
+++ web/src/app/api/utils/sql.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  process.env.VITE_SUPABASE_URL || '',
-  process.env.SUPABASE_SERVICE_ROLE_KEY || process.env.VITE_SUPABASE_ANON_KEY || ''
+  https://zvawrbajdslrkbsgyegn.supabase.co || '',
+  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp2YXdyYmFqZHNscmtic2d5ZWduIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MzIzMTMxMSwiZXhwIjoyMDc4ODA3MzExfQ.l1twPcFGg310GgOCWk7u8i4wplcM_bAAyyoC8tA5CTE || eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp2YXdyYmFqZHNscmtic2d5ZWduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMyMzEzMTEsImV4cCI6MjA3ODgwNzMxMX0.gjDQJj9TE07a3bBMbiR-AZd01ym5AIMRsHHczAlkFyg || ''
 );
 
 export default supabase;
